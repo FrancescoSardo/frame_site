49b4233192bd47d7d2ecc3bd3df9c8e31f23f4c6
FrancescoSardo <francescosardo91@gmail.com>
2023-11-08 23:11:07 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+ import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -10,13 +10,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    }),
+    adapter: adapter(),
     paths: {
       base: '/frame_site',
     },
