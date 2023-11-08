c864249c4d1c42b50759a848ad2ea9e958456563
FrancescoSardo <francescosardo91@gmail.com>
2023-11-08 23:57:40 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 
 import adapter from '@sveltejs/adapter-static';
-//import vercel from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     // base path for static assets
     paths: {
       base: '/frame_site'
